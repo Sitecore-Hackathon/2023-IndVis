98b07d49cfdf453e7cc351962e097e05e8fbbdf5
Himmat Dulawat <himmat.dulawat@mullenlowe.com>
2023-03-04 16:23:36 +0800

    Admin page created

---
+++ src/Project/MvcToHeadlessMigration.Website/sitecore/admin/MigrationModule.aspx.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.sitecore.admin;
+using System;
+
+namespace MvcToHeadlessMigration.Website.sitecore.admin
+{
+    public partial class MigrationModule : AdminPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnInit(EventArgs e)
+        {
+            //CheckSecurity will check that user should have admin rights  
+            CheckSecurity(true);
+            base.OnInit(e);
+        }
+
+        protected void btnClick_Click1(object sender, EventArgs e)
+        {
+            lblMessage.Text = ConnectionStringTextBox.Text;
+            // Do your custom code  
+        }
+    }
+}
